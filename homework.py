bab73f69a66d7d8e6127e3664ca238029a57efd7
AruzhanIbr <ibragimova080499@gmail.com>
2022-09-06 15:55:16 +0600

    Разделила слова в имени функции go_to_company_name_homepage

--- homework.py
+++ homework.py
@@ -7,12 +7,13 @@ def my_print(func_name, *args):
     print()
 
 
+
 def open_browser(browser_name):
     my_print(open_browser.__name__,  browser_name)
 
 
-def go_to_companyname_homepage(page_url):
-    my_print(go_to_companyname_homepage.__name__, page_url)
+def go_to_company_name_homepage(page_url):
+    my_print(go_to_company_name_homepage.__name__, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
@@ -20,5 +21,5 @@ def find_registration_button_on_login_page(page_url, button_text):
 
 
 open_browser("Chrome")
-go_to_companyname_homepage("https://qa.guru")
+go_to_company_name_homepage("https://qa.guru")
 find_registration_button_on_login_page("https://qa.guru/python", "Начать обучение")
